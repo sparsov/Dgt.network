f564fe7010ed38b0deb4538527d6f3e558901a7b
TsvetkovAV <tsvetkov93dr@yandex.ru>
2018-12-25 13:40:11 +0300

    Finally FIX

--- bgx/rest-api/sawtooth_rest_api/bgx_handlers.py
+++ bgx/rest-api/sawtooth_rest_api/bgx_handlers.py
@@ -323,14 +323,14 @@ class BgxRouteHandler(RouteHandler):
             LOGGER.debug('BgxRouteHandler: post_transfer to META WALLET=%s',meta_wallet)
             address_to = meta_wallet
 
-            state = await self._get_state_by_addr(request, address_to)
-            _, amount_before = self._get_token(state, address_to)
+        state = await self._get_state_by_addr(request, address_to)
+        _, amount_before = self._get_token(state, address_to)
 
         LOGGER.debug('BgxRouteHandler: post_transaction make payload=%s',payload)
         tx_status = await self._make_token_transfer(request,address_from,address_to,num_bgt,coin_code)
         # status = 202
 
-        if tx_status == 'COMMITED':
+        if tx_status == 'COMMITTED':
             state = await self._get_state_by_addr(request, address_to)
             _, amount_after = self._get_token(state, address_to)
             if amount_before == amount_after:
@@ -338,7 +338,7 @@ class BgxRouteHandler(RouteHandler):
 
         tx = {
             'timestamp': datetime.now().__str__(),
-            'status': tx_status == 'COMMITED',
+            'status': tx_status == 'COMMITTED',
             'tx_payload': num_bgt,
             'currency': payload['coin_code'],
             'address_from': address_from,
