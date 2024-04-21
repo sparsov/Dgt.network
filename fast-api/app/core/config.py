b55d057dc1fa7b11cc6a2a0578795de914583e96
sparsov <sparsov@sinergo.ru>
2024-04-22 00:01:22 +0700

    add bin fast-api

--- fast-api/app/core/config.py
+++ fast-api/app/core/config.py
@@ -5,12 +5,12 @@ env.read_env()
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "DGT API Project"
-    API_PREFIX: str = "/api/v1"
-    PROJECT_DOCS: str = "/api/docs"
+    API_PREFIX: str = env.str("API_PREFIX", default="/api/v1")
+    PROJECT_DOCS: str = env.str("PROJECT_DOCS", default="/api/docs")
     CRYPTO_BACK: str = env.str("CRYPTO_BACK", default="openssl")
     API: int = env.int("API", default=8003)
     VERSION: str = env.str("VERSION", default="0.0.1")
-    DGT_CONNECT: str = env.str("DGT_CONNECT",default="tcp://validator-dgt-c1-1:4104")
+    DGT_CONNECT : str = env.str("DGT_CONNECT",default="tcp://validator-dgt-c1-1:4104")
 
     class Config:
         env_file = ".env"
